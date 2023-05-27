7b34692c576fdb7e57b58ae75ab13c0129fa90ec
Tesar Pratama Kurnia Akbar <95579621+tesarp1812@users.noreply.github.com>
2023-05-27 11:46:47 +0700

    Create.js

---
+++ test.js
@@ -0,0 +1,29 @@
+const tek = 'berenang kok';
+const kol = 'bisa bisa kala';
+const pol = 'aku terbiasa bikin suara';
+const kac = 'biasanya aku harus berangkat pagi setiap hari untuk berangkat sekolah';
+
+const tes = ['aku hanya manusia'];
+
+var cl = kol.split(" ");
+var tl = cl.length;
+
+var tk = tek.split(" ");
+var kt = tk.length;
+
+var pl = pol.split(" ");
+var lp = pl.length;
+
+var kc = kac.split(" ");
+var ck = kc.length;
+
+var max = Math.max(tl, kt, lp, ck);
+
+
+
+
+console.log("hasil dari pol=", (tl));
+console.log("hasil dari kol=", (kt));
+console.log("hasil dari pol=", (lp));
+console.log("hasil dari kac =", (ck));
+console.log("nilai max dari ketiga kata =", (max));
